f29359cf49060a8f95399e0c6ad20b6330f3736a
SG_Demi <stevengoldbergm@gmail.com>
2023-02-18 23:44:12 -0600

    Initial App Setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import AboutMe from "./components/pages/AboutMe";
+import ContactMe from "./components/pages/ContactMe";
+import MyWork from "./components/pages/MyWork";
+import TechStack from "./components/pages/TechStack";
+
+function App() {
+  return (
+    <Layout>
+      <div>This is my starter div</div>
+      <AboutMe />
+      <TechStack />
+      <MyWork />
+      <ContactMe />
+    </Layout>
+  );
+}
+
+export default App;
