afceef426ba5e1881c564f83c00d4df7357b4940
SG_Demi <stevengoldbergm@gmail.com>
2023-02-18 23:45:23 -0600

    Set up Socials base

---
+++ src/components/layout/Socials.js
@@ -0,0 +1,18 @@
+import classes from "./Socials.module.css"
+
+function Socials() {
+  // Add a context wrapper to check for whether a contact message has been sent!
+  return (
+    <header>
+      <nav>
+        <ul className={classes.ul}>
+          <li className={classes.li}>Twitter</li>
+          <li className={classes.li}>LinkedIn</li>
+          <li className={classes.li}>GitHub</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Socials;
