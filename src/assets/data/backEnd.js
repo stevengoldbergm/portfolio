71341f5d00e2add43d828330ce560d024ab410b5
SG_Demi <stevengoldbergm@gmail.com>
2023-03-24 23:12:18 -0500

    Added back end tech stack json file

---
+++ src/assets/data/backEnd.js
@@ -0,0 +1,51 @@
+const backEnd = [
+    {
+        reference: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5https://nodejs.org/en/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg",
+        altText: "NodeJS",
+        id: 1
+    },
+    {
+        reference: 'https://expressjs.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg",
+        altText: "ExpressJS",
+        id: 2
+    },
+    {
+        reference: 'https://graphql.org/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/graphql-colored.svg",
+        altText: "GraphQL",
+        id: 3
+    },
+    {
+        reference: 'https://www.mongodb.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg",
+        altText: "MongoDB",
+        id: 4
+    },
+    {
+        reference: 'https://www.mysql.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg",
+        altText: "MySQL2",
+        id: 5
+    },
+    {
+        reference: 'https://www.postgresql.org/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg",
+        altText: "PostgreSQL",
+        id: 6
+    },
+    {
+        reference: 'https://www.heroku.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/heroku-colored.svg",
+        altText: "Heroku",
+        id: 7
+    },
+    {
+        reference: 'https://wordpress.org',
+        source: "https://cdn.worldvectorlogo.com/logos/wordpress-blue.svg",
+        altText: "WordPress",
+        id: 8
+    },
+];
+export default backEnd;
