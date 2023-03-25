986b97494c4894753cd866cf1aa5154d558de31b
SG_Demi <stevengoldbergm@gmail.com>
2023-03-24 23:12:38 -0500

    Added front end tech stack json file

---
+++ src/assets/data/frontEnd.js
@@ -0,0 +1,51 @@
+const frontEnd = [
+    {
+        reference: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
+        altText: "HTML5",
+        id: 1
+    },
+    {
+        reference: 'https://www.w3.org/TR/CSS/#css',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
+        altText: "CSS3",
+        id: 2
+    },
+    {
+        reference: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
+        altText: "JavaScript",
+        id: 3
+    },
+    {
+        reference: 'https://jquery.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/jquery-colored.svg",
+        altText: "JQuery",
+        id: 4
+    },
+    {
+        reference: 'https://reactjs.org/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg",
+        altText: "ReactJS",
+        id: 5
+    },
+    {
+        reference: 'https://sass-lang.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg",
+        altText: "SASS",
+        id: 6
+    },
+    {
+        reference: 'https://getbootstrap.com/',
+        source: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg",
+        altText: "Bootstrap",
+        id: 7
+    },
+    {
+        reference: 'https://bulma.io',
+        source: "https://cdn.worldvectorlogo.com/logos/bulma.svg",
+        altText: "Bulma",
+        id: 8
+    },
+];
+export default frontEnd;
