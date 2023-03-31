4e96e9bb9fdc3770f93545f344f3775df5630713
SG_Demi <stevengoldbergm@gmail.com>
2023-03-31 17:59:29 -0500

    Added a slide index

---
+++ src/assets/imgs/slides/slides.js
@@ -0,0 +1,17 @@
+import movieMate3 from "./MovieMate3.0.png"
+import movieMate2 from "./MovieMate2.0.png"
+import jate from "./JATE.png"
+import techBlog from "./TechBlog.png"
+import noteTaker from "./NoteTaker.png"
+import weather from "./Weather.png"
+import workdayScheduler from "./WorkdayScheduler.png"
+
+export {
+  movieMate3,
+  movieMate2,
+  jate,
+  techBlog,
+  noteTaker,
+  weather,
+  workdayScheduler
+};
