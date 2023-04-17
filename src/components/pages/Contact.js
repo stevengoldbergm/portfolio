78318d7dd0283cbff2bcce1a4478e55f77aaf2c8
SG_Demi <stevengoldbergm@gmail.com>
2023-04-16 23:28:51 -0500

    Added Contact page using the Contact Form and ContactSection components

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+// Import components
+import ContactSection from "../ContactSection/ContactSection";
+
+// Import CSS
+import "./Contact.css"
+
+// Create a Contact Me page
+function Contact(props) {
+  return (
+    <div id="contact-me" className="contact">
+      <ContactSection />
+    </div>
+  );
+}
+
+export default Contact;
