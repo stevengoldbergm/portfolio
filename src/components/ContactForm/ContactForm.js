a89f6ba4b856919028a76252847537ea4cfd694d
SG_Demi <stevengoldbergm@gmail.com>
2023-04-16 23:24:05 -0500

    Adding Contact Form component

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,64 +1,147 @@
+// Import React hooks
 import { useRef } from "react";
-import classes from "./ContactForm.module.css"
 
+// Import css
+import "./ContactForm.css"
+
+// Create component form for contact submission
 function ContactForm(props) {
-  // console.log(props);
-  // Set up the useRef objects
-  const contactFirstName = useRef();
-  const contactLastName = useRef();
-  const contactEmail = useRef();
-  const contactMessage = useRef();
+  // useRef to make easy forms. Could useState, but it's more work.
+  const fnInputRef = useRef();
+  const lnInputRef = useRef();
+  const emlInputRef = useRef();
+  const txtInputRef = useRef();
 
-  // Make the formSubmit handler
-  function submitHandler(event) {
-    event.preventDefault();
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const enteredFirstName = fnInputRef.current.value;
+    const enteredLastName = lnInputRef.current.value;
+    const enteredEmail = emlInputRef.current.value;
+    const enteredText = txtInputRef.current.value;
 
-    // define the form values
-    const enteredFirstName = contactFirstName.current.value;
-    const enteredLastName = contactLastName.current.value;
-    const enteredEmail = contactEmail.current.value;
-    const enteredMessage = contactMessage.current.value;
+    // Find the current time
+    const utcDate = Date.now();
+    const plainDate = new Date(utcDate).toLocaleString("en-US", {
+      hour12: false,
+    });
+    console.log(utcDate);
+    console.log(plainDate);
+    console.log(
+      enteredFirstName,
+      enteredLastName,
+      enteredEmail,
+      enteredText,
+      utcDate,
+      plainDate
+    );
 
-    // create data payload to push to server
-    const contactData = {
-      firstName: enteredFirstName,
-      lastName: enteredLastName,
+    // Set up the payload for the message
+    const payload = {
+      name: `${enteredFirstName} ${enteredLastName}`,
       email: enteredEmail,
-      message: enteredMessage,
+      comments: enteredText,
+      date: plainDate,
+      utcDate,
     };
 
-    // send data to server
-    console.log("Data submitted successfully \n",contactData);
-    props.onSubmit(contactData);
-
-    // Clear the form
-    contactFirstName.current.value = ""
-    contactLastName.current.value = ""
-    contactEmail.current.value = ""
-    contactMessage.current.value = ""
+    submitData(payload);
   }
 
+  function submitData(contactData) {
+    // send HTTP fetch request to post data on firebase server
+    fetch(
+      "https://portfolio-contact-193c3-default-rtdb.firebaseio.com/contacts.json",
+      {
+        method: "POST",
+        body: JSON.stringify(contactData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Clear the payload
+    fnInputRef.current.value = "";
+    lnInputRef.current.value = "";
+    emlInputRef.current.value = "";
+    txtInputRef.current.value = "";
+  }
   return (
     <>
-      <div>Contact Submit Form</div>
-
-      <form className={classes.form} onSubmit={submitHandler}>
-        <label className={classes.label}>First Name</label>
-        <input className={classes.input} type="text" id="name" ref={contactFirstName} required />
-
-        <label className={classes.label}>Last Name</label>
-        <input className={classes.input} type="text" id="name" ref={contactLastName} required />
-
-        <label>Your Email</label>
-        <input type="text" id="email" ref={contactEmail} required />
-
-
-        <label>Your Message</label>
-        <input type="textarea" id="message" ref={contactMessage} required />
-
-
-        <button>Contact me</button>
-      </form>
+      <div className="form-container">
+        <form className="contact-form">
+          {/* Top row for first name/ last name */}
+          <div className="form-row">
+            <div className="form-row-container">
+              {/* first name */}
+              <label className="form-label" htmlFor="inputFirstName">
+                First Name:
+              </label>
+              <input
+                className="form-input"
+                required
+                type="text"
+                id="inputFirstName"
+                ref={fnInputRef}
+                aria-label="First Name"
+                placeholder="John"
+                // autoComplete="nope"
+              />
+            </div>
+            {/* Last name */}
+            <div className="form-row-container">
+              <label className="form-label" htmlFor="inputLastName">
+                Last Name:
+              </label>
+              <input
+                className="form-input"
+                required
+                type="text"
+                id="inputLastName"
+                ref={lnInputRef}
+                aria-label="Last Name"
+                placeholder="Doe"
+                // autoComplete="nope"
+              />
+            </div>
+          </div>
+          {/* Email */}
+          <div className="form-row-container">
+            <label className="form-label" htmlFor="inputEmail">
+              E-mail:
+            </label>
+            <input
+              className="form-input"
+              required
+              type="text"
+              id="inputEmail"
+              ref={emlInputRef}
+              aria-label="E-mail"
+              placeholder="notadeer@example.com"
+              // autoComplete="nope"
+            />
+          </div>
+          {/* Message Text */}
+          <label htmlFor="inputText" className="form-label">
+            Comments:
+          </label>
+          <textarea
+            required
+            type="text"
+            id="inputText"
+            ref={txtInputRef}
+            aria-label="Message Text"
+            placeholder="Any comments or inquiries go here!"
+          />
+          <button
+            className="form-btn"
+            type="submit"
+            onClick={handleFormSubmit}
+          >
+            Send me a message!
+          </button>
+        </form>
+      </div>
     </>
   );
 }
