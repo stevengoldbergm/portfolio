578c17c8dd60bf670ef3f87779e3f80d436e1d72
SG_Demi <stevengoldbergm@gmail.com>
2023-04-16 23:24:19 -0500

    Added Contact Section component

---
+++ src/components/ContactSection/ContactSection.js
@@ -0,0 +1,37 @@
+// Import components
+import ContactForm from "../ContactForm/ContactForm";
+
+// Import images
+import webDev02 from '../../assets/imgs/WebdevArt02.png'
+
+// Import css
+import './ContactSection.css'
+
+function ContactSection(props) {
+  return (
+    <>
+      <div className="cs-row">
+        <div className="cs-column">
+          {/* Put text here for the contact form */}
+          <h1>Questions? Comments?</h1>
+          <br />
+          <p>
+            Feel free to{" "}
+            <a className="e-mail" href="mailto:stevengoldbergm@gmail.com">
+              e-mail me ✉
+            </a>
+            <br />
+            Or send me a message and I'll get back to you!
+          </p>
+          <ContactForm />
+        </div>
+        <div className="cs-column">
+          <img alt="Illustration of two people using laptops" src={webDev02} />
+          <div className="img-overlay" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactSection;
