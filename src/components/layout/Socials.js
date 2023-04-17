5fb2d8e9be2b3d45967befbf5a2967a5f4ba15f5
SG_Demi <stevengoldbergm@gmail.com>
2023-04-17 01:52:16 -0500

    Added Socials component for footer

--- src/components/layout/Socials.js
+++ src/components/layout/Socials.js
@@ -1,15 +1,23 @@
-import classes from "./Socials.module.css"
+// Import FA/icons
+import { faGithubSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Import css
+import './Socials.css'
 
 function Socials() {
-  // Add a context wrapper to check for whether a contact message has been sent!
   return (
-      <nav>
-        <ul className={classes.ul}>
-          <li className={classes.li}>Twitter</li>
-          <li className={classes.li}>LinkedIn</li>
-          <li className={classes.li}>GitHub</li>
-        </ul>
-      </nav>
+    <div className="socials">
+      <a href="https://www.linkedin.com/in/stevengoldbergm/">
+        <FontAwesomeIcon icon={faLinkedin}  />
+      </a>
+      <a href="https://github.com/stevengoldbergm">
+        <FontAwesomeIcon icon={faGithubSquare} />
+      </a>
+      <a href="https://twitter.com/stevengoldbergm">
+        <FontAwesomeIcon icon={faTwitterSquare} />
+      </a>
+    </div>
   );
 }
 
