42cb7b409b92bf78385b613712122fe22cf61e5d
SG_Demi <stevengoldbergm@gmail.com>
2023-04-17 19:43:10 -0500

    Added Navburger to Navbar, and secondary navbar menu for smaller screen sizes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,33 +1,39 @@
 // import "./Navbar.css";
+import Navburger from "../Navburger/Navburger";
 import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="nav">
-      <div className="bold h2">SG</div>
-      <ul className="ul">
-        <li className="li">
-          <a className="a" href="#home">
-            Home
-          </a>
-        </li>
-        <li className="li">
-          <a className="a" href="#tech-stack">
-            Tech Stack
-          </a>
-        </li>
-        <li className="li">
-          <a className="a" href="#about-me">
-            About Me
-          </a>
-        </li>
-        <li className="li">
-          <a className="a" href="#contact-me">
-            Contact Me
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="nav">
+      <div className="nav-row">
+        <div className="bold h2">SG</div>
+        <ul className="ul">
+          <li className="li">
+            <a className="a" href="#home">
+              Home
+            </a>
+          </li>
+          <li className="li">
+            <a className="a" href="#tech-stack">
+              Tech Stack
+            </a>
+          </li>
+          <li className="li">
+            <a className="a" href="#about-me">
+              About Me
+            </a>
+          </li>
+          <li className="li">
+            <a className="a" href="#contact-me">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+        <Navburger />
+
+      </div>
+      {/* Expanded nav for mobile */}
+    </div>
   );
 }
 
